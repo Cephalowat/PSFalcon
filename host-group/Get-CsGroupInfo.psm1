5acd8d5f1bcdef2dd8a59350ff30c54ae73965ab
Cephalowat <github.com+Cephalowat@cephalowat.com>
2020-04-07 15:50:15 +0200

    mod Get-CsGroupInfo to allow empty params

--- host-group/Get-CsGroupInfo.psm1
+++ host-group/Get-CsGroupInfo.psm1
@@ -42,18 +42,12 @@ function Get-CsGroupInfo {
                 accept         = 'application/json'
                 'content-type' = 'application/json'
             }
+            Uri    = '/devices/combined/host-groups/v1?limit=' + [string] $Limit + '&offset=' + [string] $Offset
         }
-        switch ($PSBoundParameters.Keys) {
+        Switch ($PSBoundParameters.Keys) {
+            'Filter' { $Param.Uri += '&filter=' + $Filter }
             'Id' { $Param['Uri'] = '/devices/entities/host-groups/v1?ids=' + ($Id -join '&ids=') }
             'Verbose' { $Param['Verbose'] = $true }
-            default { 
-                $Param['Uri'] = '/devices/combined/host-groups/v1?limit=' + [string] $Limit +
-                '&offset=' + [string] $Offset
-
-                if ($Filter) {
-                    $Param.Uri += '&filter=' + $Filter
-                }
-            }
         }
         Invoke-FalconAPI @Param
     }
