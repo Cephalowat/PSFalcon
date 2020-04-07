4b0d837b30a1c549487482a720f5b1f3e94a715e
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-04-07 13:53:33 -0700

    Create Get-FXQuota.psm1

---
+++ falconx-sandbox/Get-FXQuota.psm1
@@ -0,0 +1,27 @@
+function Get-FXQuota {
+<#
+    .SYNOPSIS
+        Get your current Falcon X quota status
+#>
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [array]
+        $Id
+    )
+    process{
+        $Param = @{
+            Uri =  '/falconx/entities/submissions/v1?ids='
+            Method = 'get'
+            Header = @{
+                accept = 'application/json'
+                'content-type' = 'application/json'
+            }
+        }
+        switch ($PSBoundParameters.Keys) {
+            'Verbose' { $Param['Verbose'] = $true }
+        }
+        Invoke-FalconAPI @Param
+    }
+}
