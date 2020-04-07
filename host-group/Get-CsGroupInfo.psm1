20fc5f7ae88fe2e83520413a48ed7104fac75b5b
Cephalowat <github.com+Cephalowat@cephalowat.com>
2020-04-07 15:48:07 +0200

    Add ParameterSet to Get-CSGroupInfo

--- host-group/Get-CsGroupInfo.psm1
+++ host-group/Get-CsGroupInfo.psm1
@@ -1,5 +1,5 @@
 function Get-CsGroupInfo {
-<#
+    <#
     .SYNOPSIS
         Search for info about Host Groups
 
@@ -15,27 +15,31 @@ function Get-CsGroupInfo {
     .PARAMETER OFFSET
         The offset to start retrieving records from [Default: 0] (when IDs are not provided)
 #>
-    [CmdletBinding()]
+    [CmdLetBinding(DefaultParameterSetName = 'Default')]
     [OutputType([psobject])]
     param(
+        [Parameter(ParameterSetName = 'Id')]
         [array]
         $Id,
 
+        [Parameter(ParameterSetName = 'noId')]
         [string]
         $Filter,
 
-        [ValidateRange(2,500)]
+        [Parameter(ParameterSetName = 'noId')]
+        [ValidateRange(2, 500)]
         [int]
         $Limit = 500,
 
+        [Parameter(ParameterSetName = 'noId')]
         [int]
         $Offset = 0
     )
-    process{
+    process {
         $Param = @{
             Method = 'get'
             Header = @{
-                accept = 'application/json'
+                accept         = 'application/json'
                 'content-type' = 'application/json'
             }
         }
