5f387377512fd7a86e0358071a5da02d70c87b86
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-04-08 10:08:51 -0700

    Update New-FXSample.psm1

--- falconx-sandbox/New-FXSample.psm1
+++ falconx-sandbox/New-FXSample.psm1
@@ -25,11 +25,6 @@ function New-FXSample {
         [boolean]
         $Confidential = $true
     )
-    begin{
-        if (Test-Path $Path) {
-            $FileData = Get-Content $Path
-        }
-    }
     process{
         $Param = @{
             Uri = '/samples/entities/samples/v2?file_name=' + (Split-Path $Path -Leaf) +
@@ -39,7 +34,7 @@ function New-FXSample {
                 accept = 'application/json'
                 'content-type' = 'application/octet-stream'
             }
-            Body = "[ " + $FileData + " ]"
+            Body = "[ " + (Get-Content $Path) + " ]"
         }
         switch ($PSBoundParameters.Keys) {
             'Comment' { $Param.Uri += '&comment=' + $Comment }
