52b4316a56a16d39285a044a701b9caadaa19863
bk-cs <54042976+bk-cs@users.noreply.github.com>
2020-04-10 22:40:39 -0700

    Update Stop-CsContain.psm1

--- hosts/Stop-CsContain.psm1
+++ hosts/Stop-CsContain.psm1
@@ -9,7 +9,7 @@ function Stop-CsContain {
     [CmdletBinding()]
     [OutputType([psobject])]
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $true)]
         [array]
         $Id
     )
